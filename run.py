d016d80ad2b5158a54df6c8543ee15c328ac7e26
Jake Lever <jlever@bcgsc.ca>
2018-07-23 22:32:08 -0700

    Fix length issue

--- run.py
+++ run.py
@@ -92,7 +92,7 @@ if __name__ == '__main__':
 
 	potentialTweets = decryptFile(key,'lines')
 	potentialTweets = [ t.strip() for t in potentialTweets ]
-	potentialTweets = [ t for t in potentialTweets if t and not t in recentTweets ]
+	potentialTweets = [ t for t in potentialTweets if t and len(t) < 280 and not t in recentTweets ]
 
 	assert len(potentialTweets) > 0
 
